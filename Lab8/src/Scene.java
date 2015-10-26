aae18f61114a797f3e28de9f7493e96fa041e1e0
lu chen <u5486612@anu.edu.au>
2015-10-26 16:04:11 +1100

    lighting using phone reflection model to ray tracing

--- Lab8/src/Scene.java
+++ Lab8/src/Scene.java
@@ -8,10 +8,16 @@ import java.util.ArrayList;
 public class Scene extends ArrayList<Sphere> {
 
 	Color background = Color.black;
+	Color ambient = new Color(0.2f,0.2f,0.2f);
+	Color lightColour= new Color(0.8f,0.8f,0.8f);
+	Color specularColour= new Color(1.0f,1.0f,1.0f);
 
 	public Color raytrace(Ray r) {
 		Sphere hit = null;
 		Double mindis = null;
+
+		// store the light source direction
+		 P3D lightingPosition = new P3D(-0.7, -0.7, 0.8);
 
 		for (Sphere s : this) {
 			Double t = s.intersect(r);
@@ -23,10 +29,28 @@ public class Scene extends ArrayList<Sphere> {
 			}
 		}
 		if (hit != null) {
-			return hit.color;
+			Double t = hit.intersect(r);
+			P3D intersetPoint = r.position.add(r.direction.scale(t));
+			P3D normal = hit.center.sub(intersetPoint).normalize();
+
+			P3D reflection = normal.scale(2 * (lightingPosition.dot(normal))).sub(lightingPosition).normalize();
+
+			double a =  0.2f;
+			double d =  0.8f * lightingPosition.normalize().dot(normal);
+			double s =  1.0f * reflection.dot(r.direction.normalize());
+
+			float value =(float) Math.max(Math.min((a + d + s), 1.0f), 0.0f);
+
+//			return hit.color;
+
+			Color finalColor = new Color((int) (hit.color.getRed() * value), (int) (hit.color.getGreen() *value),(int) (hit.color.getBlue()*value));
+
+			return finalColor;
+
 		} else {
 			return background;
 		}
+//		Intersect inter
 	}
 
 }
